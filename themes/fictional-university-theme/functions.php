c3bdbdf62252e96c972d34e47cf4a984e6f21a76
Yitzchak Lindenbaum <yitzl@hebrew-academy.org.il>
2024-01-07 16:22:55 +0200

    ironed out functions bug

--- themes/fictional-university-theme/functions.php
+++ themes/fictional-university-theme/functions.php
@@ -1,5 +1,20 @@
 <?php
 
+function pageBanner() {
+  // php logic will live here
+  ?>
+  <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php $bannerImage = get_field('page_banner_background_image'); echo $bannerImage['sizes']['pageBanner'] ?>);"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php the_title(); ?></h1>
+            <div class="page-banner__intro">
+                <p><?php the_field("page_banner_subtitle"); ?></p>
+            </div>
+        </div>
+    </div>
+  <?php 
+}
+
 function university_files() {
     wp_enqueue_script('main-univeristy-js', get_theme_file_uri('/build/index.js'), array('jquery'), 1.0, true);
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
